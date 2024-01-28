6f62fa7fc38bc0b445b5cdc7baecd88eacfc0576
Jonas <36707885+jasonski@users.noreply.github.com>
2024-01-28 22:44:35 +0100

    Added the science page with slides

---
+++ scripts.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+let timer;
+
+function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  timer = setTimeout(showSlides, 10000); // Change slide every 10 seconds
+}
+
+function plusSlides(n) {
+  clearTimeout(timer);
+  showSlides((slideIndex += 0));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showSlides();
+});
